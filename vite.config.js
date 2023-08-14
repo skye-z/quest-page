208021f09742e20c77f76fd650f806f7842f8edc
skay <skai-zhang@hotmail.com>
2023-08-14 12:14:01 +0800

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,9 +23,35 @@ export default defineConfig({
     }),
     Components({
       resolvers: [NaiveUiResolver()]
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'logo/@x1.png', 'logo/@x2.png'],
+      manifest: {
+        name: 'Quest',
+        short_name: 'Quest',
+        description: 'a ultra mini system for question bank management and mock exams',
+        theme_color: '#1f2937',
+        icons: [
+          {
+            src: 'logo/@x1.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo/@x2.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        cacheId: 'quest-cache',
+        navigateFallback: null
+      }
     })
   ],
-  server:{
+  server: {
     proxy: {
       '/api': {
         target: "http://localhost:12999/api",
